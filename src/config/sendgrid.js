64a7aa2c89486f00bdd5ca3bf9b3b6f6d2d053b8
Eric <eric.jiang@monash.edu>
2019-09-02 16:03:30 +1000

    sendgrid sandbox demo

---
+++ src/config/sendgrid.js
@@ -0,0 +1,5 @@
+const sendgridMail = require("@sendgrid/mail");
+
+sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports = sendgridMail;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
 require("dotenv").config();
-const express = require("express");
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  sendgridClient = require("./config/sendgrid"),
+  fetch64 = require("fetch-base64");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.json());
 
-app, listen();
+app.post("/api/email", async (request, response, next) => {
+  const {
+    headerImgUrl,
+    user,
+    department,
+    subject,
+    body,
+    signature,
+    templateId
+  } = request.body;
+  const { email, first_name, last_name } = user;
+
+  const attachments = [];
+  if (headerImgUrl) {
+    const headerImage = await fetch64.remote(headerImgUrl);
+    attachments.push({
+      filename: "header.png",
+      type: "image/png",
+      content: headerImage[0],
+      content_id: "header_img",
+      disposition: "inline"
+    });
+  }
+  sendgridClient
+    .send({
+      to: email,
+      from: process.env.SENDGRID_NOREPLY_EMAIL,
+      templateId: templateId || process.env.SENDGRID_DEFAULT_TEMPLATE_ID,
+      dynamic_template_data: {
+        first_name,
+        last_name,
+        subject,
+        body,
+        signature,
+        department
+      },
+      attachments
+    })
+    .then(resp => {
+      response.json({ message: resp, status: "OK" });
+    })
+    .catch(err => {
+      response.status(500).send({ status: "ERROR", message: err });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App Listening on port ${PORT}`);
+});
